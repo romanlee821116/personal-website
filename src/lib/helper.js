1d6ffdba9ccc2810757d5f50f6e623153df60c1f
Roman <romanlee1116@gmail.com>
2024-06-27 22:52:32 +0800

    change path

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -2,7 +2,7 @@
 export const getPictureRelativePath = (pic, fileType = 'png') => {
     // 如果是打包后的环境，则使用相对路径
     if (process.env.NODE_ENV === 'production') {
-        return `./assets/${pic}.${fileType}`;
+        return `./${pic}.${fileType}`;
     }
     // 在开发环境中，仍然使用绝对路径
     return `/assets/${pic}.${fileType}`;
